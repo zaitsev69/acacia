7e4271610cecb1d8926f003fa445a93fd1db427a
zaitsev69 <126803254+zaitsev69@users.noreply.github.com>
2024-07-02 16:50:20 +0200

    valeur encode

--- components/contact.jsx
+++ components/contact.jsx
@@ -16,7 +16,7 @@ const Contact = ({}) => {
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                         <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
                     </svg>
-                    <p className="ml-2 ">Rue de l'Église 84190 La Roque-Alric</p>
+                    <p className="ml-2 ">Rue de l&apos;Église 84190 La Roque-Alric</p>
                 </div>
 
             </div>
